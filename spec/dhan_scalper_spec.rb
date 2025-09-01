b56b6abd2d663f15e29f474c12f3ecfc36dfa902
Shubham Taywade <shubhamtaywade82@gmail.com>
2025-09-01 16:19:10 +0530

    Initial Commit

---
+++ spec/dhan_scalper_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe DhanScalper do
+  it "has a version number" do
+    expect(DhanScalper::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
